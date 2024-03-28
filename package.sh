55cb9d70c500dbf54103ab1ad6cd956d44b9aa07
Jackson Goode <jacksongoode@proton.me>
2024-03-27 21:08:16 -0700

    Fix

--- package.sh
+++ package.sh
@@ -18,9 +18,5 @@ yarn install && yarn build && npx @grafana/sign-plugin@latest
 cp -r dist nline-plotlyjs-panel
 zip -r nline-plotlyjs-panel.zip nline-plotlyjs-panel
 mv nline-plotlyjs-panel.zip nline-plotlyjs-panel-"${tag}".zip
-if ! [ -x "$(command -v md5)" ]; then
-	md5sum nline-plotlyjs-panel-"${tag}".zip >nline-plotlyjs-panel-"${tag}".zip.md5
-else
-	md5 nline-plotlyjs-panel-"${tag}".zip >nline-plotlyjs-panel-"${tag}".zip.md5
-fi
+md5sum nline-plotlyjs-panel-"${tag}".zip >nline-plotlyjs-panel-"${tag}".zip.md5
 rm -rf nline-plotlyjs-panel
